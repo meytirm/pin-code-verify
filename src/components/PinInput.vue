a95e067de5b650d22820f054032137505c30c4f8
Mehdi <mahdi0131rain@gmail.com>
2022-01-22 16:53:55 +0330

    refactor

--- src/components/PinInput.vue
+++ src/components/PinInput.vue
@@ -2,75 +2,102 @@
   <div class="wrapper">
     <input
       class="pin-input col-2 border-radius-8 q-py-xs bg-tes-grey-light"
-      @input="inputMovement"
       v-for="(digit, index) in digits"
-      pattern="\d*"
-      maxlength="1"
-      :data-index="index"
       :key="digit"
+      type="text"
+      @input="movement"
+      @keydown="moveBack"
+      maxlength="1"
       :ref="setInputRef"
+      :data-index="index"
     />
   </div>
 </template>
 
-<script>
-export default {
-  name: "PinInput",
-  data() {
-    return {
-      inputRefs: [],
-    };
-  },
-  emits: ["submitCode"],
-  props: {
-    digits: {
-      default: 4,
-      type: Number,
-    },
-  },
-  mounted() {
-    this.inputRefs[0].focus();
-  },
-  methods: {
-    setInputRef(el) {
-      if (el) {
-        this.inputRefs.push(el);
-      }
-    },
-    inputMovement(e) {
-      const currentInputIndex = Number(e.target.attributes["data-index"].value);
-      const typeOfInput = e.inputType;
-      const allInputs = this.inputRefs;
-      const stepMoveInput = 1;
-      const code = this.inputRefs.map((input) => input.value).join("");
+<script lang="ts">
+import { defineComponent, ref, onMounted } from 'vue'
 
-      // enter number
-      if (typeOfInput === "insertText") {
-        // check if next input available
-        if (allInputs[currentInputIndex + stepMoveInput]) {
-          allInputs[currentInputIndex + stepMoveInput].focus();
-          if (code.length === this.digits) {
-            this.submitCode(code);
-          }
-        } else {
-          if (code.length === this.digits) {
-            this.submitCode(code);
+export default defineComponent(
+  {
+    name: "PinInput",
+    emits: ["submitCode"],
+    props: {
+      digits: {
+        default: 4,
+        type: Number,
+      },
+    },
+    setup(props, { emit }) {
+      const inputRefs = ref<HTMLInputElement[]>([])
+      function setInputRef(el: HTMLInputElement) {
+        if (el && inputRefs.value.length < props.digits) {
+          inputRefs.value.push(el)
+        }
+      }
+
+      const moveBack = function (event: KeyboardEvent) {
+        const target = event.target as HTMLInputElement
+        const inputIndex = target.getAttribute('data-index')
+        if (event.key === 'Backspace' && Number(inputIndex) > 0) {
+          setTimeout(() => {
+            const previousInput = inputRefs.value[Number(inputIndex) - 1]
+            previousInput.focus()
+            previousInput.select()
+          }, 100)
+        }
+      }
+
+      const movement = function (event: InputEvent) {
+        const value = event.data
+        const target = event.target as HTMLInputElement
+        if (value) {
+          if (isNumber(value)) {
+            const inputIndex = target.getAttribute('data-index')
+            if (inputIndex) {
+              if (allowBackspace(event, inputIndex)) {
+                const nextInput = inputRefs.value[Number(inputIndex) + 1]
+                nextInput.focus()
+              }
+            }
+            const code = inputRefs.value.map((input) => input.value).join('')
+            // emit('update:pin-code', code)
+            if (code.length === props.digits) {
+              sendCode(code)
+            }
+          } else {
+            event.preventDefault()
+            target.value = ''
           }
         }
       }
 
-      // backspace
-      if (typeOfInput === "deleteContentBackward") {
-        if (allInputs[currentInputIndex - stepMoveInput]) {
-          allInputs[currentInputIndex - stepMoveInput].focus();
-        }
+      function isNumber(value: string) {
+        return Number.isInteger(parseInt(value))
+      }
+
+      function allowBackspace(event: InputEvent, inputIndex: string) {
+        return (
+          event.inputType !== 'deleteContentBackward' && Number(inputIndex) < 3
+        )
+      }
+
+      function sendCode(code: string) {
+        emit('submitCode', code)
+      }
+
+      onMounted(() => {
+        inputRefs.value[0].focus()
+      })
+
+      return {
+        movement,
+        moveBack,
+        setInputRef,
       }
     },
-    submitCode(code) {
-      this.$emit("submitCode", code);
-    },
-  },
-};
+  }
+
+)
 </script>
 
 <style lang="css" scoped>
